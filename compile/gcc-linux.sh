2169fb27a3ea6e55eb306c07e6f6d4e984649d22
Jukka Suomela <jukka.suomela@iki.fi>
2014-12-15 14:16:36 +0200

    g++ 4.9.1, Linux.

---
+++ compile/gcc-linux.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+mkdir -p bin tmp || exit 1
+cd src || exit 1
+exec g++ -Wall -Wextra -Wold-style-cast -Woverloaded-virtual -Wsign-promo -Wdouble-promotion -Wconversion -Wsign-conversion -Wmissing-declarations -Wredundant-decls -Wno-unused-parameter -Wno-float-equal -Wno-array-bounds -march=native -O3 -fopenmp -std=c++11 -DNDEBUG mf2d.cc driver.cc filter.cc imageio.cc -lcfitsio -o ../bin/mf2d

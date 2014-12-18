397e21e384a5904afe1c5ac2d497dc808af20bba
Jukka Suomela <jukka.suomela@iki.fi>
2014-12-18 21:08:02 +0200

    More tests.

--- src/mf2d-unittest.cc
+++ src/mf2d-unittest.cc
@@ -1,12 +1,14 @@
+#include <cmath>
 #include <cstdlib>
 #include <iostream>
+#include <limits>
 #include "filter.h"
 
 template <typename T>
 static void check(int x, int y, int hx, int hy, const T* in, T* out, const T* expected) {
     median_filter<T>(x, y, hx, hy, 0, in, out);
     for (int i = 0; i < x*y; ++i) {
-        if (out[i] != expected[i]) {
+        if (!(out[i] == expected[i] || (std::isnan(out[i]) && std::isnan(expected[i])))) {
             std::cerr << "mismatch: " << i << ": " << out[i] << " vs. " << expected[i] << "\n";
             std::exit(EXIT_FAILURE);
         }
@@ -14,7 +16,7 @@ static void check(int x, int y, int hx, int hy, const T* in, T* out, const T* ex
 }
 
 template <typename T>
-static void test() {
+static void test1() {
     const int x = 10;
     const int y = 5;
     T H = 0.5;
@@ -98,6 +100,103 @@ static void test() {
     check(x, y, 1, 1, in, out, exp11);
     check(x, y, 2, 2, in, out, exp11);
     check(x, y, 4, 4, in, out, zero);
+}
+
+template <typename T>
+static void test2() {
+    const int x = 10;
+    const int y = 5;
+    T X = std::numeric_limits<T>::quiet_NaN();
+    T H = 0.5;
+    T in[x * y] = {
+        0,0,0,1,1,1,0,0,0,0,
+        0,0,0,1,1,1,0,0,0,0,
+        0,0,0,1,1,X,0,0,0,0,
+        0,0,0,1,1,1,0,0,2,0,
+        0,0,0,1,1,1,0,0,0,0
+    };
+    T exp01[x * y] = {
+        0,0,0,1,1,1,0,0,0,0,
+        0,0,0,1,1,1,0,0,0,0,
+        0,0,0,1,1,1,0,0,0,0,
+        0,0,0,1,1,1,0,0,0,0,
+        0,0,0,1,1,1,0,0,1,0
+    };
+    T exp10[x * y] = {
+        0,0,0,1,1,1,0,0,0,0,
+        0,0,0,1,1,1,0,0,0,0,
+        0,0,0,1,1,H,0,0,0,0,
+        0,0,0,1,1,1,0,0,0,1,
+        0,0,0,1,1,1,0,0,0,0
+    };
+    T exp11[x * y] = {
+        0,0,0,1,1,1,0,0,0,0,
+        0,0,0,1,1,1,0,0,0,0,
+        0,0,0,1,1,1,0,0,0,0,
+        0,0,0,1,1,1,0,0,0,0,
+        0,0,0,1,1,1,0,0,0,0
+    };
+    T out[x * y];
+    check(x, y, 0, 0, in, out, in);
+    check(x, y, 0, 1, in, out, exp01);
+    check(x, y, 1, 0, in, out, exp10);
+    check(x, y, 1, 1, in, out, exp11);
+}
+
+template <typename T>
+static void test3() {
+    const int x = 10;
+    const int y = 5;
+    T X = std::numeric_limits<T>::quiet_NaN();
+    T H = 0.5;
+    T in[x * y] = {
+        0,0,0,1,1,X,X,X,X,X,
+        0,0,0,1,1,1,X,X,X,X,
+        0,0,0,1,1,0,0,X,X,X,
+        0,0,0,1,1,1,0,0,X,X,
+        0,0,0,1,1,1,0,0,0,X
+    };
+    T exp01[x * y] = {
+        0,0,0,1,1,1,X,X,X,X,
+        0,0,0,1,1,H,0,X,X,X,
+        0,0,0,1,1,1,0,0,X,X,
+        0,0,0,1,1,1,0,0,0,X,
+        0,0,0,1,1,1,0,0,0,X
+    };
+    T exp10[x * y] = {
+        0,0,0,1,1,1,X,X,X,X,
+        0,0,0,1,1,1,1,X,X,X,
+        0,0,0,1,1,0,0,0,X,X,
+        0,0,0,1,1,1,0,0,0,X,
+        0,0,0,1,1,1,0,0,0,0
+    };
+    T exp11[x * y] = {
+        0,0,0,1,1,1,1,X,X,X,
+        0,0,0,1,1,1,0,0,X,X,
+        0,0,0,1,1,1,0,0,0,X,
+        0,0,0,1,1,1,0,0,0,0,
+        0,0,0,1,1,1,0,0,0,0
+    };
+    T exp22[x * y] = {
+        0,0,0,H,1,1,1,0,0,X,
+        0,0,0,1,1,1,1,0,0,0,
+        0,0,0,1,1,1,1,0,0,0,
+        0,0,0,1,1,1,H,0,0,0,
+        0,0,0,1,1,1,0,0,0,0
+    };
+    T out[x * y];
+    check(x, y, 0, 0, in, out, in);
+    check(x, y, 0, 1, in, out, exp01);
+    check(x, y, 1, 0, in, out, exp10);
+    check(x, y, 1, 1, in, out, exp11);
+    check(x, y, 2, 2, in, out, exp22);
+}
+
+template <typename T>
+static void test() {
+    test1<T>();
+    test2<T>();
+    test3<T>();
 }
 
 int main() {

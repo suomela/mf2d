fbbf2e8f96f419eb0c7a6de22e244728a2435ebf
Jukka Suomela <jukka.suomela@iki.fi>
2014-12-19 01:29:54 +0200

    Tweak parameters a bit.

--- src/param.h
+++ src/param.h
@@ -7,18 +7,20 @@ const int MAX_H_2D = 100;
 #define BLOCK_SIZES_1D 64,128,256,512,1024,2048,4096
 #define BLOCK_SIZES_2D 16,32,64,128,256,512
 
+// Reasonable values based on benchmarks
+
 inline int choose_blocksize_1d(int h) {
-    if (h < 16) {
+    if (h < 10) {
         return 64;
-    } else if (h < 32) {
+    } else if (h < 20) {
         return 128;
-    } else if (h < 64) {
+    } else if (h < 40) {
         return 256;
-    } else if (h < 128) {
+    } else if (h < 80) {
         return 512;
-    } else if (h < 256) {
+    } else if (h < 160) {
         return 1024;
-    } else if (h < 512) {
+    } else if (h < 240) {
         return 2048;
     } else {
         return 4096;
@@ -26,7 +28,6 @@ inline int choose_blocksize_1d(int h) {
 }
 
 inline int choose_blocksize_2d(int h) {
-    // Reasonable values based on benchmarks
     if (h < 3) {
         return 16;
     } else if (h < 5) {

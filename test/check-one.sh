83e819dbb7dd304acfd912ccf20bf88912fb6992
Jukka Suomela <jukka.suomela@iki.fi>
2014-12-19 14:33:08 +0200

    A bit more flexible.

--- test/check-one.sh
+++ test/check-one.sh
@@ -3,10 +3,10 @@
 time test/run-one.sh "$1" || exit 1
 echo
 if [ "$(uname)" == "Darwin" ]; then
-    md5 tmp/*.fits > "tmp/checksum-$1" || exit 1
-    cmp "test/checksum-md5" "tmp/checksum-$1" || exit 1
+    md5 tmp/*.fits > "tmp/checksum" || exit 1
+    cmp "test/checksum-md5" "tmp/checksum" || exit 1
 else
-    md5sum tmp/*.fits > "tmp/checksum-$1" || exit 1
-    cmp "test/checksum-md5sum" "tmp/checksum-$1" || exit 1
+    md5sum tmp/*.fits > "tmp/checksum" || exit 1
+    cmp "test/checksum-md5sum" "tmp/checksum" || exit 1
 fi
 
